c72191d8b66bfe1c3d6d2f8b44c384cd99da4076
Gaetano Zappalà <gaetanozappala@zappi.homenet.telecomitalia.it>
2020-08-17 17:29:38 +0200

    New version

---
+++ precache-manifest.b273debd02e048f91d9682e8e197dcc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc471cdaa10325a17ed10a49fc7ec76",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "a556c90386964e64a3b2",
+    "url": "/homepage/static/css/main.feab315a.chunk.css"
+  },
+  {
+    "revision": "ccc3dca555ccb28071f9",
+    "url": "/homepage/static/js/2.cb5f70be.chunk.js"
+  },
+  {
+    "revision": "622221136d49693d21d7de2ed6f074f4",
+    "url": "/homepage/static/js/2.cb5f70be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a556c90386964e64a3b2",
+    "url": "/homepage/static/js/main.496b10c8.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  },
+  {
+    "revision": "f34b1ce0eca8cdbb680273aa2ce6bd9e",
+    "url": "/homepage/static/media/macBookPro.f34b1ce0.svg"
+  }
+]);
